937e29d4978c00301baf2a6f6ffd959f8b74fbfd
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2023-01-05 18:24:20 +0700

    upload

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -6,6 +6,7 @@ const courseController = require('../app/controlers/CourseController');
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
+router.post('/:handle-form-actions', courseController.handleFormAction);
 router.put('/:id', courseController.update);
 router.patch('/:id/restore', courseController.restore);
 router.delete('/:id', courseController.destroy);
